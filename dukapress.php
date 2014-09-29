ae1c7be09312c8973f67bcd10af1a1c346f5832d
rixeo <rixeo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-29 14:02:33 +0000

    Tagging version 2.5.1
    git-svn-id: https://plugins.svn.wordpress.org/dukapress/trunk@998558 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dukapress.php
+++ dukapress.php
@@ -2,13 +2,13 @@
 /*
 Plugin Name: DukaPress Shopping Cart
 Description: DukaPress Shopping Cart
-Version: 2.5
+Version: 2.5.1
 Author: Rixeo and Nickel Pro
 Author URI: http://dukapress.org/
 Plugin URI: http://dukapress.org/
 */
 
-$dp_version = 2.5;
+$dp_version = 2.51;
 
 session_start();
 define('DP_PLUGIN_URL', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)));
